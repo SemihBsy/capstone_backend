92b8c91c43ce3e89553ffcb461bc91a3cf10194a
SemihB <semihr5171@gmail.com>
2023-03-18 19:00:43 -0400

    user

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+import userRoutes from './routes/users.js'
 
 const app = express();
+dotenv.config();
 
-const { PORT = 8000 } = process.env;
+// database config
+const MONGO = process.env.MONGO;
 
-app.get('/', (req, res) => {
-    res.send("Hello!")
+// Connect to the database
+mongoose.set('strictQuery', false);
+mongoose.connect(MONGO, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
 });
 
+// Connection message handler
+mongoose.connection
+.on("open", () => console.log("Connected to mongodb"))
+.on("close", () => console.log("Disconnected from mongodb"))
+.on("error", (error) => console.log(error))
+
+app.use("/api/users", userRoutes);
+
+const PORT =  process.env.PORT ?? 8000
 app.listen(PORT, () => {
     console.log(`Listening on PORT ${PORT}`);
 });
