2eb614ca85a8436b4d2af055cd7d70c2e881f84d
SemihB <semihr5171@gmail.com>
2023-03-20 21:52:05 -0400

    follow/unfollow

--- controllers/user.js
+++ controllers/user.js
@@ -34,7 +34,7 @@ export const deleteUser = async (req, res, next) => {
     if (req.params.id === req.user.id) {
        try {
         await User.findByIdAndDelete(req.params.id);
-        
+
         res.status(200).json('User deleted successfully');
        } catch (err) {
         next(err);
@@ -42,5 +42,48 @@ export const deleteUser = async (req, res, next) => {
     } else {
         return next(handleError(403, "You can only update your account"));
     }
+};
+
+export const follow = async (req, res, next) => {
+       try {
+        // user
+        const user = await User.findById(req.params.id);
+        // current user
+        const currentUser = await User.findById(req.body.id);
+        
+        if (!user.followers.includes(req.body.id)) {
+            await user.updateOne({
+                $push: { followers: req.body.id },
+            });
+            await currentUser.updateOne({ $push: { following: req.params.id } });
+        } else {
+            res.status(403).json("You already follow this user");
+        }
+        res.status(200).json("User added to following");
+       } catch (err) {
+        next(err);
+       } 
+};
+
+export const unFollow = async (req, res, next) => {
+    try {
+     // user
+     const user = await User.findById(req.params.id);
+     // current user
+     const currentUser = await User.findById(req.body.id);
+     
+     if (currentUser.following.includes(req.params.id)) {
+         await user.updateOne({
+             $pull: { followers: req.body.id },
+         });
+
+         await currentUser.updateOne({ $pull: { following: req.params.id } });
+     } else {
+         res.status(403).json("You already not following this user");
+     }
+     res.status(200).json("User removed from following");
+    } catch (err) {
+     next(err);
+    } 
 };
 
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getUser, updateUser, deleteUser } from "../controllers/user.js";
+import { getUser, updateUser, deleteUser, follow, unFollow } from "../controllers/user.js";
 import { verifyToken } from "../verifyToken.js";
 
 const router = express.Router();
@@ -12,5 +12,12 @@ router.get('/find/:id', getUser);
 
 // Delete User
 router.delete('/:id', verifyToken, deleteUser);
+
+// Follow
+router.put('/follow/:id', verifyToken, follow);
+
+// Unfollow
+router.put('/unfollow/:id', verifyToken, unFollow);
+
 
 export default router;
