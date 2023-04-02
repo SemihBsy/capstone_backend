614cf523e17635b05d2fbc5f878e7192a9807e57
SemihB <semihr5171@gmail.com>
2023-04-01 22:36:19 -0400

    backend

--- controllers/auth.js
+++ controllers/auth.js
@@ -15,7 +15,7 @@ export const signup = async (req, res, next) => {
 
     const { password, ...othersData } = newUser._doc;
     res
-      .cookie("access_token", token, { httpOnly: true, secure: false, sameSite: 'none' })
+      .cookie("access_token", token, { httpOnly: true })
       .status(200)
       .json(othersData);
   } catch (err) {
@@ -38,7 +38,7 @@ export const signin = async (req, res, next) => {
     const { password, ...othersData } = user._doc;
 
     res
-      .cookie("access_token", token, { httpOnly: true, secure: false, sameSite: 'none' })
+      .cookie("access_token", token, { httpOnly: true })
       .status(200)
       .json(othersData);
   } catch (err) {
